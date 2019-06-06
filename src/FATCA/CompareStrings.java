eb9fae25e642c7ee5f6a56b3c56738da079b4304
Oleg <SolOleg86@gmail.com>
2019-06-06 18:56:48 +0200

    new and updated files

---
+++ src/FATCA/CompareStrings.java
@@ -0,0 +1,18 @@
+package FATCA;
+
+import Tests.Calendar;
+
+public class CompareStrings {
+    public static void main(String[] args) {
+        String string1 = "北京市海淀区东升科技园B2-5";
+        String string2 = "北京市海淀区东升科技园B2-5";
+//        String string3 = "99999999999999";
+//        System.out.println(string1.equals(string2));
+//        System.out.println(string3.length());
+//        Calendar calendar = new Calendar();
+//        for (int i = 0; i <  ; i++) {
+//
+//        }
+        System.out.println(string1.equals(string2));
+    }
+}
