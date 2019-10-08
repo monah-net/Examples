a5e5573dfc10c082b67fe5204bfedc840ed02d19
olegsolodovnikov <SolOleg86@gmail.com>
2019-10-08 19:09:16 +0200

    new replace function for conditions

---
+++ src/FATCA/Replace.java
@@ -0,0 +1,10 @@
+package FATCA;
+
+public class Replace {
+    public static void main(String[] args) {
+        String before = "<CONDITION><CONDITION type=\"SINGULAR_CONCRETE\"><COLUMN><ALIAS>root</ALIAS><COLNAME>birth_country_code</COLNAME></COLUMN><OPERATION>!=</OPERATION><VALUE/></CONDITION><COPULA>OR</COPULA><CONDITION type=\"EXPRESSION_SINGULAR\"><COLUMN><ALIAS>root</ALIAS><COLNAME>birth_country_code</COLNAME></COLUMN><OPERATION>IS NOT</OPERATION><VALUE>\n" +
+                "NULL</VALUE></CONDITION></CONDITION>";
+        String after = before.replaceFirst("<CONDITION><CONDITION type=\"SINGULAR_CONCRETE\"><COLUMN><ALIAS>","").replaceAll("</ALIAS><COLNAME>",".").replaceAll("</COLNAME></COLUMN><OPERATION>"," ").replaceAll("</OPERATION><VALUE/></CONDITION><COPULA>"," '' ").replaceAll("</COPULA><CONDITION type=\"EXPRESSION_SINGULAR\"><COLUMN><ALIAS>"," ").replaceAll("</OPERATION><VALUE>"," ").replaceAll("</VALUE></CONDITION></CONDITION>"," ");
+        System.out.println(after);
+    }
+}
