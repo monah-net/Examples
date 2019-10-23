48dc8ac53d85f3d8ce511722eeb59cd009d12ce4
monah-net <SolOleg86@gmail.com>
2019-10-23 22:10:37 +0200

    test

--- src/FATCA/ReadXMLFile2.java
+++ src/FATCA/ReadXMLFile2.java
@@ -15,7 +15,7 @@ public class ReadXMLFile2 {
 
         try {
 
-            File file = new File("/Users/olegsolodovnikov/Desktop/Files/xmlFilesForTesting/AU2018AUR5TS2T.00307.SG.AGG.20160831180224038.xml");
+            File file = new File("D:\\xmlFiles\\AU2018AUR5TS2T.00307.SG.AGG.20160831180224038.xml");
 
             DocumentBuilder dBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
 
---
+++ src/FATCA/XMLParsing.java
@@ -0,0 +1,53 @@
+package FATCA;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+
+public class XMLParsing {
+    public static void main(String[] args) {
+        String result = null;
+        try {
+
+            File file = new File("D:\\xmlFiles\\R5TS2T.00307.SG.2302016083118022401ORACLE.xml");
+
+            DocumentBuilder dBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            Document doc = dBuilder.parse(file);
+            NodeList nodeList = doc.getElementsByTagName("crs:AccountReport");
+            if (doc.hasChildNodes()) {
+                getNode(nodeList);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    private static void getNode(NodeList nodelist) {
+        for (int counter = 0; counter < nodelist.getLength(); counter++) {
+            Node nodeTemp = nodelist.item(counter);
+            if (nodeTemp.getNodeType() == Node.ELEMENT_NODE) {
+                if (nodeTemp.getChildNodes().getLength() != 1){
+                    System.out.println(nodeTemp.getNodeName() + "[OPEN]");
+                }else{
+                    System.out.println(nodeTemp.getNodeName() + " : element value = " + nodeTemp.getTextContent() + "[OPEN]");
+                }
+                if (nodeTemp.hasAttributes()) {
+                    NamedNodeMap nodeMap = nodeTemp.getAttributes();
+                    for (int mapCounter = 0; mapCounter < nodeMap.getLength(); mapCounter++) {
+                        Node nodeTempMap = nodeMap.item(mapCounter);
+                        System.out.println("attr name : " + nodeTempMap.getNodeName());
+                        System.out.println("attr value : " + nodeTempMap.getNodeValue());
+                    }
+                }
+                if (nodeTemp.hasChildNodes()) {
+                    getNode(nodeTemp.getChildNodes());
+                }
+                System.out.println(nodeTemp.getNodeName() + "[CLOSE]");
+            }
+        }
+    }
+}
