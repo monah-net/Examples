2f3d7dce44012e3d3023618f2b06e797227579e8
olegsolodovnikov <SolOleg86@gmail.com>
2019-10-25 23:08:11 +0200

    Merge branch 'master' of https://github.com/monah-net/Examples new class bitches

--- src/FATCA/XMLComparator.java
+++ src/FATCA/XMLComparator.java
@@ -7,37 +7,41 @@ import org.w3c.dom.NodeList;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 public class XMLComparator {
     public static void main(String[] args) {
         try {
-
-            File file = new File("D:\\xmlFiles\\JP\\JP2016USR5TS2T.00307.SG.230.20160831180224001.xml");
-            File file2 = new File("D:\\xmlFiles\\JP\\JP2016USR5TS2T.00307.SG.230.20160831180224051ORACLE.xml");
+            Map<String , String> resultMap = new HashMap<>();
+            File file = new File("/Users/olegsolodovnikov/Desktop/Files/xmlFilesForTesting/R5TS2T.00307.SG.2302016083118022401Test.xml");
+            File file2 = new File("/Users/olegsolodovnikov/Desktop/Files/xmlFilesForTesting/R5TS2T.00307.SG.2302016083118022401ORACLETest.xml");
             DocumentBuilder dBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
             DocumentBuilder dBuilder2 = DocumentBuilderFactory.newInstance().newDocumentBuilder();
             Document doc = dBuilder.parse(file);
             Document doc2 = dBuilder2.parse(file2);
             NodeList nodelist = doc.getElementsByTagName("ftc:AccountReport");
             NodeList nodeList2 = doc2.getElementsByTagName("ftc:AccountReport");
-            Node node = nodelist.item(10);
-            Node node1 = nodeList2.item(10);
-            System.out.println(node.getChildNodes().item(1).getChildNodes().item(1).getNextSibling().getTextContent());
-            System.out.println(node.getNodeType() == Node.ELEMENT_NODE);
-            System.out.println(node1.getNodeType() == Node.ELEMENT_NODE);
-            System.out.println(node.getTextContent().equals(node1.getTextContent()));
+            for (int i = 0; i < nodelist.getLength(); i++) {
+                resultMap.put("AccountReportNo" + i,"");
+            }
             for (int counter = 0; counter < nodelist.getLength(); counter++) {
                 if (nodelist.item(counter).getNodeType() == Node.ELEMENT_NODE) {
                     for (int counter2 = 0; counter2 < nodeList2.getLength(); counter2++) {
                         if (nodeList2.item(counter2).getNodeType() == Node.ELEMENT_NODE) {
-                            if (nodelist.item(counter).getTextContent().replaceAll("\\<DocRefId\\>[0-9A-Za-z\\.]*\\<\\/ftc:DocRefId\\>", "").trim().equals(nodeList2.item(counter2).getTextContent().replaceAll("\\<DocRefId\\>[0-9A-Za-z\\.]*\\<\\/ftc:DocRefId\\>", "").trim())) {
-                                System.out.println("Account number" + counter);
-                                System.out.println(counter2);
-                                System.out.println(nodelist.item(counter).getTextContent().replaceAll("\\<DocRefId\\>[0-9A-Za-z\\.]*\\<\\/ftc:DocRefId\\>", "").trim());
+                            System.out.println(nodelist.item(counter).getTextContent().replaceAll("\n","") + "[CLOSE]");
+                            System.out.println(nodeList2.item(counter2).getTextContent().replaceAll("\n","") + "[CLOSE]");
+                            if (nodelist.item(counter).getTextContent().replaceAll("\n","").trim().equals(nodeList2.item(counter2).getTextContent().replaceAll("\n","").trim())) {
+                                resultMap.replace("AccountReportNo" + counter,"DONE");
+                                break;
                             }
                         }
                     }
                 }
+            }
+            for (Map.Entry <String, String> pair : resultMap.entrySet()
+                 ) {
+                System.out.println(pair.getKey() + " : " + pair.getValue());
             }
         }
         catch(Exception e){
