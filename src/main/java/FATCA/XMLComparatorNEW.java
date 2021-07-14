5066bec8b62fc76ee3a99bfc77f9198798d5dc57
monah-net <SolOleg86@gmail.com>
2021-07-14 07:32:36 +0300

    new comparator, for UK. universal type of code.

--- src/main/java/FATCA/XMLComparatorNEW.java
+++ src/main/java/FATCA/XMLComparatorNEW.java
@@ -1,6 +1,7 @@
 package FATCA;
 
 import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import org.apache.commons.codec.binary.StringUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
@@ -17,10 +18,10 @@ import java.util.stream.Collectors;
 
 class XML_comparatorNEW {
     public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
-        ArrayList resElementsIn = new ArrayList();
-        ArrayList resElementsOut = new ArrayList();
-        File inputXmlFile = new File("C:\\Users\\osolodovnikov\\Desktop\\TEMP_FOLDER\\UK\\XML_Files\\origin_fatca_det_uk_TEST_Lineriased_Linear.xml");
-        File outputXmlFile = new File("C:\\Users\\osolodovnikov\\Desktop\\TEMP_FOLDER\\UK\\XML_Files\\origin_fatca_det_uk_TEST.xml");
+        ArrayList resElementsIn = new ArrayList<String>();
+        ArrayList resElementsOut = new ArrayList<String>();
+        File inputXmlFile = new File("/Users/olegsolodovnikov/Desktop/XML_Files/id001/JP2016USR5TS2T.00307.SG.230.20210625123100001.xml");
+        File outputXmlFile = new File("/Users/olegsolodovnikov/Desktop/XML_Files/id001/JP2016USR5TS2T.00307.SG.230.20210625123100001.xml");
         DocumentBuilder dBuilderInput = DocumentBuilderFactory.newInstance().newDocumentBuilder();
         Document javaParsedInput = dBuilderInput.parse(inputXmlFile);
         Document javaParsedOutput = dBuilderInput.parse(outputXmlFile);
@@ -36,7 +37,7 @@ class XML_comparatorNEW {
                 StringBuilder attribute = new StringBuilder();
                 if (current.hasAttributes()) {
                     NamedNodeMap currAttrs = current.getAttributes();
-                    List currAttrTempList = new ArrayList<>();
+                    List currAttrTempList = new ArrayList<String>();
                     for (int j = 0; j < currAttrs.getLength(); j++) {
                         currAttrTempList.add("/" + currAttrs.item(j).getNodeName() + "=" + currAttrs.item(j).getNodeValue());
                     }
@@ -119,5 +120,7 @@ class XML_comparatorNEW {
         System.out.println(resElementsOut);
         System.out.println(resElementsIn.isEmpty());
         System.out.println(resElementsOut.isEmpty());
+        StringUtils.equals("000713141414","00071311141414");
+        System.out.println(StringUtils.equals("000713141414","00071311141414"));
     }
 }
